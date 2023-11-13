f3804c0508a9cb531ca2fdc9b3fabffcc2b182eb
shoumikdey <shoumikdey@Shoumiks-MacBook-Air.local>
2023-11-13 22:03:55 +0100

    Code documentation with comments

--- src/app.py
+++ src/app.py
@@ -10,12 +10,15 @@ import cv2.aruco as aruco
 import pygame
 pygame.init()
 
+# Initialize the video capture object
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 cap.set(cv2.CAP_PROP_FPS, 30)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 time.sleep(5)
+
+# Initialize font and other parameters for displaying text on the frame
 font = cv2.FONT_HERSHEY_SIMPLEX
 font_scale = 1
 font_color = (255, 255, 255)  # White color in BGR
@@ -23,9 +26,14 @@ font_thickness = 2
 frame_rate = 30
 prev = time.time()
 
-
+# Initialize ARUCO markers dictionary and parameters
 aruco_dict = aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
 arucoParams =  aruco.DetectorParameters()
+
+# Functions to interact with Kuksa and control the vehicle
+# ...
+
+# Main function to perform face authentication and control the vehicle
 
 def set_vss_value(vss, value):
     with VSSClient('192.168.1.99', 55556) as client:
@@ -48,6 +56,17 @@ def main():
     verified = 0
     drunk = 0
     playing_aud = 0
+
+      # Functions to set flags indicating verification, drunkenness, and audio playing status
+    # ...
+
+    # Functions to check the status of flags
+    # ...
+
+    # Functions to perform specific actions on the vehicle
+    # ...
+
+    # Function to play audio
 
     def setDrunk():
         global drunk
@@ -72,23 +91,20 @@ def main():
         return playing_aud == 1
     
     def car_action_verified():
-        # set_vss_value("Vehicle.Window.Row2.DriverSide.Action", "open")
-        # set_vss_value("Vehicle.Window.Row1.DriverSide.Action", "open")
-        # set_vss_value("Vehicle.Door.Row1.DriverSide.Action", "open")
-        print('verified action')
-
+        set_vss_value("Vehicle.Window.Row2.DriverSide.Action", "open")
+        set_vss_value("Vehicle.Window.Row1.DriverSide.Action", "open")
+        set_vss_value("Vehicle.Door.Row1.DriverSide.Action", "open")
+        
     def car_action_drunk():
-        # set_vss_value("Vehicle.Window.Row2.DriverSide.Action", "open")
-        # set_vss_value("Vehicle.Door.Row2.DriverSide.Action", "open")
-        print('drunk action')
-
+        set_vss_value("Vehicle.Window.Row2.DriverSide.Action", "open")
+        set_vss_value("Vehicle.Door.Row2.DriverSide.Action", "open")
+       
     def car_action():
-        # set_vss_value("Vehicle.Window.Row2.DriverSide.Action", "close")
-        # set_vss_value("Vehicle.Window.Row1.DriverSide.Action", "close")
-        # set_vss_value("Vehicle.Door.Row1.DriverSide.Action", "close")
-        # set_vss_value("Vehicle.Door.Row2.DriverSide.Action", "close")
-        print('secure action')
-
+        set_vss_value("Vehicle.Window.Row2.DriverSide.Action", "close")
+        set_vss_value("Vehicle.Window.Row1.DriverSide.Action", "close")
+        set_vss_value("Vehicle.Door.Row1.DriverSide.Action", "close")
+        set_vss_value("Vehicle.Door.Row2.DriverSide.Action", "close")
+        
     def playAudio():
         my_sound2 = pygame.mixer.Sound(os.path.abspath('src'+os.sep+'alexa2.wav'))
         my_sound2.set_volume(1.0)
@@ -96,11 +112,20 @@ def main():
         my_sound = pygame.mixer.Sound(os.path.abspath('src'+os.sep+'welcome.wav'))
         my_sound.set_volume(0.5)
         my_sound.play()
+    
+    # Function to authenticate a face using OpenCV and DeepFace
 
-    def authenticate_face(data, frame):
+    def authenticate_face(frame):
+
+        # Detect faces using Haar cascade classifier
+
         face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
         faces = face_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=5)
         cv2.putText(frame, str(len(faces)), (50, 50), font, font_scale, font_color, font_thickness)
+        
+        # Detect ARUCO markers
+
+        
         corners, ids, rejected = aruco.detectMarkers(
             frame, aruco_dict, parameters=arucoParams
         )
@@ -109,21 +134,27 @@ def main():
             if ids[0][0] == 0:
                 setDrunk()
 
+         # If faces are detected and not yet verified, attempt verification
+
         if len(faces) > 0 and verified == 0:
             cv2.putText(frame, 'Verifying', (100, 50), font, 2, font_color, font_thickness)
 
+            # Verify the face using DeepFace library
+
             if DeepFace.verify(img1_path=os.path.abspath('src'+os.sep+'test.jpg'), img2_path=frame, enforce_detection=False)['verified'] == True:
-                print(isVerified())
                 setVerified()
-                print(isVerified())
                 return frame
+            
         return frame
+
+    # Main loop for capturing frames, performing authentication, and controlling the vehicle
 
     while(True):
         ret, frame = cap.read()
         if (time.time() - prev) > 1 / frame_rate:
-            data = cv2.resize(frame, (299, 299))
-            frame = authenticate_face(frame, frame)
+            frame = authenticate_face(frame)
+
+            # If verified, perform corresponding actions
 
             if isVerified():
                 cv2.putText(frame, 'Verified', (100, 50), font, 2, font_color, font_thickness)
@@ -131,17 +162,25 @@ def main():
                 if isAudioPlaying() != 1:
                     playAudio()
                     setAudio()
+
+            # If drunk, perform corresponding actions
+
               
             elif isDrunk():
                 car_action_drunk()
+
+            # If neither verified nor drunk, perform default actions
 
             else:
                 car_action()
 
             cv2.imshow("Display", frame)        
 
+            # Break the loop if 'q' is pressed
+
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+    # Release the video capture object and close all windows
 
     cap.release()
     cv2.destroyAllWindows()
